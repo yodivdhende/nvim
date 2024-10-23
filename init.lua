95428b3e79b100099731ff778774f8f185c1cd87
Yvandenhende <yodi.vandenhende@nowjobs.com>
2024-10-23 14:13:11 +0200

    settup cattpuccin

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
 vim.cmd(":imap jj <esc>")
 vim.cmd(":set relativenumber")
 vim.cmd(":set tabstop=2")
+vim.cmd(":set softtabstop=2")
+vim.cmd(":set shiftwidth=2")
+
+require("config.lazy")
+require("lazy").setup("plugins")
