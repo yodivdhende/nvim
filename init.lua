b0d7b73208e06335973ae604cc25629fe8b3d215
Yvandenhende <yodi.vandenhende@nowjobs.com>
2024-10-23 15:35:20 +0200

    adding telescope

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ vim.cmd(":set relativenumber")
 vim.cmd(":set tabstop=2")
 vim.cmd(":set softtabstop=2")
 vim.cmd(":set shiftwidth=2")
+vim.g.mapleader = " "
 
 require("config.lazy")
 require("lazy").setup("plugins")
+
