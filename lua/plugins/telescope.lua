7df8f5474e9f33b5d1765dd32effb43577405214
Yodi <yodi.vandenhende@gmail.com>
2024-10-25 13:15:01 +0200

    adding neo tree

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,5 +7,6 @@ return {
 				--vim.keymap.set('n', '<C-S-p>', builtin.live_grep, { desc = 'Telescope live grep' })
 				vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 				vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+				vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'Telescope list reference' })
 			end,
 }
