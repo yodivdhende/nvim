ce67788aec24afb91d74a26f8782c233e223ff11
Yodi <yodi.vandenhende@gmail.com>
2024-10-27 18:55:42 +0100

    adding snippits

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,23 +1,39 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		lazy = false,
 		config = function()
 			require("mason").setup()
 		end
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls"}
+				ensure_installed = { "lua_ls" }
 			})
 		end
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.eslint.setup({
+				capabilities = capabilities,
+			})
+
 			vim.api.nvim_create_autocmd('LspAttach', {
 				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
 				callback = function()
