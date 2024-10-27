b5c3b48be557e69cb991efc7587a0a999357855f
Yodi <yodi.vandenhende@gmail.com>
2024-10-27 18:18:34 +0100

    adding alphanvim

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd(":set relativenumber")
 vim.cmd(":set tabstop=2")
 vim.cmd(":set softtabstop=2")
 vim.cmd(":set shiftwidth=2")
+vim.cmd(":set nowrap")
 vim.g.mapleader = " "
 
 require("config.lazy")
