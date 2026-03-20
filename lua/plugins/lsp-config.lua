e59f876bee47296aff27602d56cd118ff8f434f0
yodi vandenhende <yodi.vandenhende@gmail.com>
2026-03-20 11:25:16 +0100

    fixing issues with merge

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,33 +1,33 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"eslint",
-					"html",
-					"svelte",
-				}
-			})
-		end
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
-			local lspconfig = require("lspconfig")
+  {
+    "mason-org/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "eslint",
+          "html",
+          "svelte",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
 
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
