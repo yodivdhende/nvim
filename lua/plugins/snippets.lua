bfd8eb5cf6348eec50b7c2c86fe6e33f54a2acc8
yodi vandenhende <yodi.vandenhende@gmail.com>
2026-03-23 11:18:45 +0100

    fixing snippets

---
+++ lua/plugins/snippets.lua
@@ -0,0 +1,9 @@
+return {
+  "L3MON4D3/LuaSnip",
+  opts = function(_, opts)
+    require("luasnip.loaders.from_vscode").lazy_load({
+      paths = { vim.fn.stdpath("config") .. "/lua/snippets" },
+    })
+    return opts
+  end,
+}
