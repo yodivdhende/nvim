89dc57b342b72eea674235bf982de426d8b77dde
yodivdhende <yodi.vandenhende@gmail.com>
2026-03-24 17:06:58 +0100

    fixing autocomplete css

---
+++ lua/plugins/css.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "css-lsp" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        cssls = {},
+      },
+    },
+  },
+}
