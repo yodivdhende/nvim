96a5557bfd3c4887f8509486dd2c21df219c0fdf
Yodi Vandenhende <yodi.vandenhende@MacBook-Pro-van-Yodi.local>
2026-04-01 22:28:35 +0200

    adjusting treesitter for .svelte files

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,3 +8,16 @@ return {
     },
   },
 }
+
+--return {
+--  -- add gruvbox
+--  { "rebelot/kanagawa.nvim" },
+--
+--  -- Configure LazyVim to load gruvbox
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = "kanagawa-wave",
+--    },
+--  },
+--}
