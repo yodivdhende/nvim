2cab788ea44c16d6efe3758b8f446c32de8f67f1
Yodi <yodi.vandenhende@gmail.com>
2026-05-26 22:01:56 +0200

    fixing Markdown

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,53 +1,19 @@
 return {
-  -- Let LazyVim manage mason/mason-lspconfig; just ensure servers are installed
   {
     "mason-org/mason.nvim",
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "svelte-language-server", "typescript-language-server", "eslint-lsp", "html-lsp" })
+      vim.list_extend(opts.ensure_installed, {
+        "svelte-language-server",
+        "typescript-language-server",
+        "eslint-lsp",
+        "html-lsp",
+      })
     end,
   },
 
-  -- Configure LSP servers via opts (LazyVim merges these cleanly)
   {
     "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.eslint.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.svelte.setup({
-				capabilities = capabilities,
-			})
-
-			vim.api.nvim_create_autocmd('LspAttach', {
-				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-				callback = function()
-					vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-					vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-					vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-				end
-			})
-		end
-	},
-    init = function()
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local bufnr = args.buf
-          local opts = { noremap = true, silent = true, buffer = bufnr }
-          vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-        end,
-      })
-    end,
     opts = {
       servers = {
         svelte = {
@@ -61,13 +27,13 @@ return {
             },
           },
         },
-        -- ts_ls resolves $lib and other tsconfig path aliases in .svelte files
         ts_ls = {
           init_options = {
             plugins = {
               {
-                name = "@svelte/ts-plugin",
-                location = vim.fn.expand("$HOME/.local/share/nvim/mason/packages/svelte-language-server/node_modules/@svelte/ts-plugin"),
+                name = "typescript-svelte-plugin",
+                location = vim.fn.expand("$HOME/.local/share/nvim/mason/packages/svelte-language-server/node_modules/typescript-svelte-plugin"),
+                languages = { "svelte" },
               },
             },
           },
@@ -81,5 +47,13 @@ return {
         html = {},
       },
     },
+    init = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local o = { noremap = true, silent = true, buffer = args.buf }
+          vim.keymap.set("n", "gl", vim.diagnostic.open_float, o)
+        end,
+      })
+    end,
   },
 }
